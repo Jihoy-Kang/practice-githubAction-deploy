f521c98f7d0512ab33bff8ff0a3c029cfc24b57f
Jihyo Kang <luxuryjihyo@naver.com>
2022-08-08 14:05:15 +0900

    first commit

---
+++ src/test/java/com/jdnc/practicegithubActiondeploy/PracticeGithubActionDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdnc.practicegithubActiondeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubActionDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
